1f1539d5cf32adbfe6ea523d8ca01cd9d285f96f
btmccollum <brad.mccollum@hey.com>
2021-01-03 23:46:03 -0600

    update crkbd files

--- keyboards/crkbd/keymaps/rallie/rules.mk
+++ keyboards/crkbd/keymaps/rallie/rules.mk
@@ -1,6 +1,22 @@
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-AUDIO_ENABLE = yes
-RGBLIGHT_ENABLE = no
-OLED_DRIVER_ENABLE = yes
-LTO_ENABLE = yes
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGBLIGHT_ANIMATIONS = no
+WPM_ENABLE = no
+MOUSEKEY_ENABLE = no
+LEADER_ENABLE = no
+
+LTO_ENABLE = yes           # Enable Link Time Optimation, reduce compile size
+
+TAP_DANCE_ENABLE = yes
+STENO_ENABLE = no
+BOOTMAGIC_ENABLE =no
+TERMINAL_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = yes
+KEY_LOCK_ENABLE = no
+AUDIO_ENABLE = no
+CONSOLE_ENABLE = no
+VELOCIKEY_ENABLE = no
+SWAP_HANDS_ENABLE = no
