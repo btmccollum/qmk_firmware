c87da117531ca1d5ab56af3bfaf5cd5073cd86e9
btmccollum <brad.mccollum@hey.com>
2021-02-28 22:07:46 -0600

    rebase

--- keyboards/eyeohdesigns/babyv/keymaps/rallie/keymap.c
+++ keyboards/eyeohdesigns/babyv/keymaps/rallie/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,       KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,       KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,       KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-        KC_LCTL,                   KC_LGUI, KC_SPC,           KC_ENT,     MO(NUMBRS), KC_(SYMBLS),               KC_RALT
+        KC_LCTL,                   KC_LGUI, KC_SPC,           KC_ENT,     MO(NUMBRS), MO(SYMBLS),                KC_RALT
     ),
     [FUNCTN] = LAYOUT_2u_1u(
         KC_VOLU, KC_Q,    KC_PGUP, RGB_TOG, BL_STEP, KC_T,    KC_Y,       KC_U,       XXXXXXX,   RGB_MOD,  RGB_RMOD, KC_DEL,
